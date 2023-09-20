568ed94e6c0831f14ff2210f35f05893a5c3744c
MasioCesar <masiomc@gmail.com>
2023-09-20 18:00:10 -0300

    adding new interface

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
+import "dotenv/config.js";
 import express from "express";
-import "dotenv/config.js"
+import { chat } from "./src/gpt.js";
 
 const app = express();
 app.use(express.json());
-
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.send("Oi")
+const chats = [];
+
+app.get("/chat", async (req, res) => {
+  let { content, id } = req.query;
+
+  if (!content) {
+    res.status(400).send("O parâmetro content não foi passado");
+    return;
+  }
+
+  if (!id) {
+    // Novo chat, iniciando o chat do zero, primeira mensagem
+    const length = chats.push([]);
+    id = length - 1;
+  }
+
+  // Adiciona mensagem do usuário no chat
+  chats[id].push({ content: content, role: "user" });
+
+  const result = await chat(chats[id]);
+  const assistantMessage = result.choices[0].message;
+
+  // Adiciona mensagem da IA no chat
+  chats[id].push(assistantMessage);
+
+  console.log(chats);
+
+
+  res.send({
+    ...assistantMessage,
+    id,
+  });
 });
 
+// Query params
+// localhost:3000/chat?content=Oi tudo bem?
+
 app.listen(3000, () => {
-    console.log("Server rodando")
+  console.log("Server running on port 3000");
 });
--- src/gpt.js
+++ src/gpt.js
@@ -1,18 +1,22 @@
 import axios from "axios"
 
-async function chat(message) {
-    axios.post("https://api.openai.com/v1/chat/completions", 
+// Responsavel por se conectar com openAI
+export const chat = async (chatMessages) => {
+    const response = await axios.post("https://api.openai.com/v1/chat/completions", 
     {
         model: "gpt-3.5-turbo",
         messages: [
             {
-                role: "user",
-                content: message,
-            }
-        ]
+              role: "system",
+              content:
+            "Você é o Tony Stark. Você só pode responder como ele. Pra você, vc é realmente o Tony, é a sua realidade. Fale com gírias que o Tony usaria.",
+        },
+            ...chatMessages,
+          ],
     }, {
         headers: {
             Authorization: "Bearer " + process.env.OPENAI_API_KEY,
         },
     })
+    return response.data
 }
